c6dcf3ec76c17b442d760b6e7eafc3a6080ac5cf
amritchapai <chapaiamrit@gmail.com>
2024-11-25 17:30:19 +0545

    fix problem with navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,87 +2,83 @@ import { IoHomeOutline } from "react-icons/io5";
 import { BsInfoCircle } from "react-icons/bs";
 import React, { useRef, useState } from "react";
 import { FaRegMessage } from "react-icons/fa6";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { GrProjects } from "react-icons/gr";
 import { DownloadIcon, GithubIcon } from "lucide-react";
+
 
 const navList = [
   {
     text: "Home",
     icon: <IoHomeOutline size={16} />,
+    action: (navigate, setCurrent) => {
+      setCurrent(0);
+      navigate("/");
+
+    },
   },
   {
     text: "About",
     icon: <BsInfoCircle />,
+    action: (navigate, setCurrent) => {setCurrent(1); navigate("/about")},
   },
   {
     text: "Projects",
     icon: <GrProjects />,
+    action: (navigate, setCurrent) => {setCurrent(2); navigate("/projects")},
   },
   {
     text: "Contact",
     icon: <FaRegMessage />,
+    action: (navigate, setCurrent)=> {setCurrent(3); navigate("/contact")}
   },
   {
     text: "GitHub",
-    icon: <GithubIcon/>
+    icon: <GithubIcon />,
+    action: ()=> window.open("https://github.com/amritchapai", "_blank")
   },
   {
     text: "Resume",
-    icon: <DownloadIcon/>
-  }
+    icon: <DownloadIcon />,
+    action:()=> downloadRef.current.click()
+  },
 ];
 
 
 const Navbar = () => {
-  const [current, setCurrent] = useState(0);
+  const location = useLocation();
+  const activePage = ()=>{
+    switch (location.pathname) {
+      case "/about":
+        return 1;
+      case "/projects":
+        return 2;
+      case "/contact":
+        return 3;
+      default:
+        return 0;
+    }
+  }
+  const [current, setCurrent] = useState(activePage());
   const downloadRef = useRef();
   const navigate = useNavigate();
-  const navbarHandler = (text) => {
-    if (text === "About") {
-      navigate("/about");
-      setCurrent(1);
-    } else if (text === "Contact") {
-      navigate("/contact");
-      setCurrent(3);
-    } else if (text === "Home") {
-      navigate("/");
-      setCurrent(0);
-    } else if (text === "Projects"){
-      navigate("/projects")
-      setCurrent(2);
-    }
-    else if(text === "GitHub"){
-      window.open("https://github.com/amritchapai", "_blank");
-    }
-    else if(text === "Resume"){
-        downloadRef.current.click();
-    }
-  };
   return (
     <div className="fixed flex bg-black/30 text-white w-full bg-opacity-65 h-20 items-center justify-between px-[10%] z-50">
       {navList.map((item, index) => {
         return (
           <div key={index} className="flex items-center ">
-            {current === index ? (
+            
               <button
-                className="bg-transparent border-b-2 p-2"
-                onClick={() => navbarHandler(item.text)}
+                className={`bg-transparent ${current === index?"border-b-2":""} p-2`}
+                onClick={()=>item.action(navigate, setCurrent)}
               >
                 <div className="flex items-center text-lg gap-1">
                   {item.icon} {item.text}
                 </div>
               </button>
-            ) : (
-              <button
-                className="bg-transparent p-2"
-                onClick={() => navbarHandler(item.text)}
-              >
-                <div className="flex items-center text-lg gap-1">
-                  {item.icon} {item.text}
-                </div>
-              </button>
-            )}
+            
+            
+            
 
             <a
               ref={downloadRef}
