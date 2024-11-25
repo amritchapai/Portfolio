10980a0a528be55f984c4191c2bbc7cd5a2c02d5
amritchapai <chapaiamrit@gmail.com>
2024-11-25 18:00:41 +0545

    sad

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,24 @@
-import './App.css'
-import {createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Main from './Components/Main'
-import About from './Components/About'
-import Contact from './Components/Contact'
-import Projects from './Components/Projects'
-import Body from './Components/Body'
-
-const browserRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <Main />,
-    children: [
-      {
-        path: "/",
-        element: <Body />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      {
-        path: "/projects",
-        element: <Projects />,
-      },
-    ],
-  },
-]);
+import './App.css';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Use HashRouter
+import Main from './Components/Main';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Projects from './Components/Projects';
+import Body from './Components/Body';
 
 function App() {
   return (
-    <RouterProvider router={browserRouter}/>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route path="/" element={<Body />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
