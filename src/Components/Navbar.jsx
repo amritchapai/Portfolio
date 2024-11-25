fc2817b3da26b32d0dc8ef7fd926503aa8aeab36
amritchapai <chapaiamrit@gmail.com>
2024-11-25 15:27:38 +0545

    improved navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,10 @@
 import { IoHomeOutline } from "react-icons/io5";
 import { BsInfoCircle } from "react-icons/bs";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { FaRegMessage } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 import { GrProjects } from "react-icons/gr";
-import { GithubIcon } from "lucide-react";
+import { DownloadIcon, GithubIcon } from "lucide-react";
 
 const navList = [
   {
@@ -26,24 +26,37 @@ const navList = [
   {
     text: "GitHub",
     icon: <GithubIcon/>
+  },
+  {
+    text: "Resume",
+    icon: <DownloadIcon/>
   }
 ];
 
 
 const Navbar = () => {
+  const [current, setCurrent] = useState(0);
+  const downloadRef = useRef();
   const navigate = useNavigate();
   const navbarHandler = (text) => {
     if (text === "About") {
       navigate("/about");
+      setCurrent(1);
     } else if (text === "Contact") {
       navigate("/contact");
+      setCurrent(3);
     } else if (text === "Home") {
       navigate("/");
+      setCurrent(0);
     } else if (text === "Projects"){
       navigate("/projects")
+      setCurrent(2);
     }
     else if(text === "GitHub"){
-      window.location.href = "https://github.com/amritchapai";
+      window.location.href = "https://github.com/amritchapai"
+    }
+    else if(text === "Resume"){
+        downloadRef.current.click();
     }
   };
   return (
@@ -51,11 +64,32 @@ const Navbar = () => {
       {navList.map((item, index) => {
         return (
           <div key={index} className="flex items-center ">
-            <button className="bg-transparent border-b-2 p-2" onClick={()=>navbarHandler(item.text)}>
-              <div className="flex items-center text-lg gap-1">
-                {item.icon} {item.text} 
-              </div>
-            </button>
+            {current === index ? (
+              <button
+                className="bg-transparent border-b-2 p-2"
+                onClick={() => navbarHandler(item.text)}
+              >
+                <div className="flex items-center text-lg gap-1">
+                  {item.icon} {item.text}
+                </div>
+              </button>
+            ) : (
+              <button
+                className="bg-transparent p-2"
+                onClick={() => navbarHandler(item.text)}
+              >
+                <div className="flex items-center text-lg gap-1">
+                  {item.icon} {item.text}
+                </div>
+              </button>
+            )}
+
+            <a
+              ref={downloadRef}
+              className="hidden"
+              href="../assets/cv.pdf"
+              download="amritResume.pdf"
+            ></a>
           </div>
         );
       })}
